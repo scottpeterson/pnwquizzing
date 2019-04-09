4ad7ae757d450729ca2ef7fbc23963d7db58d4ff
Gryphon Shafer <gryphon@goldenguru.com>
2019-04-09 07:32:50 -0700

    Web tool for secret management complete

--- lib/PnwQuizzing/Control/Tool.pm
+++ lib/PnwQuizzing/Control/Tool.pm
@@ -1,9 +1,20 @@
 package PnwQuizzing::Control::Tool;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use parent 'PnwQuizzing';
+use Role::Tiny::With;
+
+with 'PnwQuizzing::Role::Secret';
 
 sub hash ($self) {
-    # TODO
+    $self->stash(
+        payload =>
+        (
+            ( $self->param('action') eq 'secret' ) ?
+                join( "\n", map { $self->secret($_) } split( /\r?\n/, $self->param('payload') ) ) :
+            ( $self->param('action') eq 'transcode' ) ? $self->transcode( $self->param('payload') ) :
+            ( $self->param('action') eq 'translate' ) ? $self->translate( $self->param('payload') ) : ''
+        )
+    );
 }
 
 1;
